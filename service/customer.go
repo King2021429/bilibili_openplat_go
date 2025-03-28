61c981ba3146030c3d7bf824c56df95573ff5298
shenyue <jiangfeng03@bilibili.com>
2025-03-28 15:55:33 +0800

    upgrade

--- service/customer.go
+++ service/customer.go
@@ -1,54 +1,66 @@
 package service
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"log"
 	"openplat/dao"
 	"openplat/model"
+	"os"
 )
 
 // ConversationSendMsg 客服系统 消息发送
 func ConversationSendMsg(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ConversationSendMsgUrl
-	queryReq := model.OpenMarketCustomerSendMsgReq{
-		MsgType:        1,
-		ConversationId: 115957154487296,
-		Msg:            "{\"content\":\"888\"}",
-		UserOpenId:     "",
-	}
-	queryReqJson, _ := json.Marshal(queryReq)
-	resp, err = dao.ApiRequest(string(queryReqJson), url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
 	if err != nil {
-		fmt.Printf("SendMsg err:%+v", err)
+		log.Fatalf("读取输入时出错: %v", err)
 	}
-
-	// 解析返回值
-	//queryResp := &model.QueryResp{}
-	//err = json.Unmarshal(resp.Data, &queryResp)
-	//if err != nil {
-	//	fmt.Printf("SendMsg resp json unmarshal err:%+v", err)
-	//}
-	//if queryResp == nil {
-	//	fmt.Println("queryResp Data nil")
-	//}
-	fmt.Printf("queryResp:%+v\n", resp)
-	return
+	reqJson = reqJson[:len(reqJson)-1]
+	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ConversationCustomerUserFrom 客服系统 获取用户来源
 func ConversationCustomerUserFrom(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ConversationCustomerUserFrom
-	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
+	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ConversationStaffStatusUpdate 客服系统 修改客服状态
 func ConversationStaffStatusUpdate(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ConversationStaffStatusUpdate
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ConversationClose 客服系统 关闭
 func ConversationClose(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ConversationClose
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
--- service/shop.go
+++ service/shop.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"openplat/dao"
 	"openplat/model"
+	"os"
 )
 
 // ShopInfoGetUrl 获取店铺信息
@@ -14,42 +18,91 @@ func ShopInfoGetUrl(clientId, accessToken, appSecret string, reqJson string) (re
 // ProductAdd 商品发布
 func ProductAdd(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ProductAddUrl
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // CommodityItemList 查询商品列表
 func CommodityItemList(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.CommodityItemListUrl
+
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ProductGetPublishRule 查询发品配置
 func ProductGetPublishRule(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ProductGetPublishRuleUrl
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ProductDetail 获取商品详情
 func ProductDetail(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ProductDetail
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ProductEdit 编辑商品
 func ProductEdit(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ProductEdit
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ProductDel 删除商品
 func ProductDel(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ProductDel
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // ProductGetCateProperty 获取商品分类属性
 func ProductGetCateProperty(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.ProductGetCateProperty
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
@@ -62,113 +115,265 @@ func CommodityCategoryQualifiedList(clientId, accessToken, appSecret string, req
 // OrderBatchDecrypt 订单敏感信息解密
 func OrderBatchDecrypt(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.OrderBatchDecrypt
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // OrderReview 流量卡审核信息回传
 func OrderReview(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.OrderReview
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // OrderRemark 商家备注
 func OrderRemark(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.OrderRemark
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // OrderDetail 订单详情查询
 func OrderDetail(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.OrderDetail
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // OrderSearchList 订单列表查询
 func OrderSearchList(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.OrderSearchList
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // LogisticsAdd 发货回掉
 func LogisticsAdd(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.LogisticsAdd
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // LogisticsEdit 物流编辑
 func LogisticsEdit(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.LogisticsEdit
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // LogisticsCompanyList 获取快递公司列表
 func LogisticsCompanyList(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.LogisticsCompanyList
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AddressCreate 地址库创建
 func AddressCreate(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AddressCreate
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AddressList 批量查询地址库列表
 func AddressList(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AddressList
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AddressGetProvince 获取全量省份信息
 func AddressGetProvince(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AddressGetProvince
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AddressGetAreasByProvince 根据省获取全量三级地址
 func AddressGetAreasByProvince(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AddressGetAreasByProvince
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // LogisticsFreightTemplateList 运费模板列表查询
 func LogisticsFreightTemplateList(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.LogisticsFreightTemplateList
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AfterSaleQueryList 售后列表查询
 func AfterSaleQueryList(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AfterSaleQueryList
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AfterSaleQueryDetail 售后详情查询
 func AfterSaleQueryDetail(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AfterSaleQueryDetail
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodGet, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AfterSaleCheckAfterSale 售后审核
 func AfterSaleCheckAfterSale(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AfterSaleCheckAfterSale
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AfterSaleConfirmReceipt 确认收货
 func AfterSaleConfirmReceipt(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AfterSaleConfirmReceipt
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AfterSaleBarterShip 换货发货
 func AfterSaleBarterShip(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AfterSaleBarterShip
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
 
 // AfterSaleStop 售后终止
 func AfterSaleStop(clientId, accessToken, appSecret string, reqJson string) (resp model.BaseResp, err error) {
 	url := model.AfterSaleStop
+	reader := bufio.NewReader(os.Stdin)
+	// 输入 access_token
+	fmt.Print("请输入reqJson串: ")
+	reqJson, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("读取输入时出错: %v", err)
+	}
+	reqJson = reqJson[:len(reqJson)-1]
 	return dao.ApiRequest(reqJson, url, model.MethodPost, clientId, accessToken, appSecret, model.BiliVersionV2)
 }
